fe14092f0b186c3ba8cba304997a3c5d4bba7a32
marcis.arajums <marcis.arajums@edigard.no>
2022-12-11 19:15:52 +0200

    Initial commit

---
+++ src/main/java/com/example/pokerss/PokerssApplication.java
@@ -0,0 +1,13 @@
+package com.example.pokerss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokerssApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokerssApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pokerss/pokerss/PokerssApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pokerss.pokerss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokerssApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
